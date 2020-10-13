1420502e63016960436704abb5fbfcf4775f8a0a
Chris Grimmett <chris@grimtech.net>
2020-10-13 05:07:08 -0700

    add mastodon link and emoji

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
         background_color: `#ffcccc`,
         theme_color: `#ffcccc`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/blossom-outline.png`, // This path is relative to the root of the site.
       },
     },
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,12 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Box>
       <Heading margin={{bottom: "5px"}}>Sakura Blossom Trading Post</Heading>
-      <Paragraph size="large">Dealer of <a target="_blank" href="http://p-memories.com/">Precious Memories</a> anime trading cards.<br/><b>On a misson to bring Prememo to the USA.</b></Paragraph>
-      <Paragraph><i>Customer Rewards Site Coming Q1 2021</i></Paragraph>
-      <Paragraph>Until then, browse our entire card inventory on <a target="_blank" href="https://www.ebay.com/str/SakuraBlossomTradingPost">eBay</a></Paragraph>
-      <Paragraph>Learn more about Precious Memories (in English) on the <a target="_blank" href="https://wiki.sbtp.xyz">SBTP Wiki</a></Paragraph>
-      <Paragraph>See our daily tweets featuring Precious Memories cards on <a target="_blank" href="https://twitter.com/ebay_sbtp">Twitter</a></Paragraph>
-      <Paragraph>For all inquiries, shoot an e-mail to <a target="_blank" href="mailto:sbtp@warpmail.net">sbtp@warpmail.net</a> or DM <strong>CJ Crispy#6052</strong> on Discord</Paragraph>
+      <Paragraph size="large">Dealer of <a target="_blank" href="http://p-memories.com/">Precious Memories</a> anime trading cards.<br/><b>On a misson to bring #Prememo to the USA.</b></Paragraph>
+      <Paragraph><i>✨ Customer Rewards Site Coming Q1 2021 ✨</i></Paragraph>
+      <Paragraph>🛒 Browse our entire card inventory on <a target="_blank" href="https://www.ebay.com/str/SakuraBlossomTradingPost">eBay</a></Paragraph>
+      <Paragraph>📜 Learn more about Precious Memories (in English) on the <a target="_blank" href="https://wiki.sbtp.xyz">SBTP Wiki</a></Paragraph>
+      <Paragraph>🐦 See our daily tweets featuring Precious Memories cards on <a target="_blank" href="https://twitter.com/ebay_sbtp">Twitter</a> and <a target="_blank" href="https://wxw.moe/@ebay_sbtp">Mastodon</a></Paragraph>
+      <Paragraph>💬 For all inquiries, e-mail <a target="_blank" href="mailto:chris@sbtp.xyz">chris@sbtp.xyz</a> or DM <strong>CJ Crispy#6052</strong> on Discord</Paragraph>
     </Box>
   </Layout>
 )
